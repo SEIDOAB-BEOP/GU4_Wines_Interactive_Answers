a0f535307c95dcc028686c72ee230b2bedafe1aa
Martin <martin_lenart@icloud.com>
2023-10-10 13:31:27 +0200

    initial commit

---
+++ GU4_Wines_Interactive/Program.cs
@@ -0,0 +1,51 @@
+﻿
+using Helpers;
+namespace _05_Wines_Interfaces;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("GU4_Wines_Interactive!");
+
+        var rnd = new csSeedGenerator();
+        WineCellar wineCellar = new WineCellar("Martin's cellar");
+
+        #region Add wines to the winecellar
+        wineCellar.Add(new csWine().Seed(rnd));
+        wineCellar.Add(new stWine().Seed(rnd));
+        #endregion
+
+        Console.WriteLine($"\nWinecellar: {wineCellar.Name}");
+        //wineCellar.Wines = null;
+        //wineCellar.Wines[0] = new csWine();
+        Console.WriteLine(wineCellar);
+
+        Console.WriteLine($"Nr of bottles: {wineCellar.Count}");
+        Console.WriteLine($"Value of winecellar: {wineCellar.Value:N2} Sek");
+
+        var hilo = wineCellar.WineHiLoCost();
+        Console.WriteLine($"\nMost expensive wine:\n{hilo.hicost}");
+        Console.WriteLine($"Least expensive wine:\n{hilo.locost}");
+
+        Console.WriteLine("\nIndexers");
+        for (int i = 0; i < wineCellar.Count; i++)
+        {
+            Console.WriteLine(wineCellar[i]);
+        }
+    }
+}
+
+/* Exercises
+1. Modify code in Main() so the interaction with a user becomes menu driven
+    1 - Give the winecellar a name
+    2 - List with winecellar
+    3 - Add one random wine bottle to the winecellar
+    4 - Show the value of the winecellar
+    5 - Show the hi and lo cost wines
+    6 - Remove all the wines from the winecellar
+    Q - Quit program
+
+2. Implement all the menufunctions above, using csConsoleInput Helpers
+*/
+
---
+++ GU4_Wines_Interactive/csWineCellar.cs
@@ -0,0 +1,68 @@
+﻿using System;
+namespace _05_Wines_Interfaces
+{
+    public class WineCellar
+    {
+        public string Name { get; set; }
+        private List<IWine> _wines = new List<IWine>();
+
+
+        public void Add(IWine wine) => _wines.Add(wine);
+        public int Count => _wines.Count;
+        public IWine this[int idx] => _wines[idx];
+ 
+        public decimal Value
+        {
+            get
+            {
+                decimal _sum = 0M;
+                foreach (var wine in _wines)
+                {
+                    _sum += wine.Price;
+                }
+                return _sum;
+            }
+        }
+
+        public override string ToString()
+        {
+            var sRet = "";
+            foreach (var wine in _wines)
+            {
+                sRet += $"{wine}\n   - {wine.GetType().Name}\n";
+            }
+            return sRet;
+        }
+
+        public WineCellar() { }
+        public WineCellar(string name)
+        {
+            Name = name;
+        }
+
+        public (IWine hicost, IWine locost) WineHiLoCost()
+        {
+            if (_wines.Count == 0) return (null,null);
+
+            decimal _hiPrice = decimal.MinValue;
+            IWine _hiWine = null;
+            decimal _loPrice = decimal.MaxValue;
+            IWine _loWine = null;
+            foreach (var wine in _wines)
+            {
+                if (wine.Price > _hiPrice)
+                {
+                    _hiWine = wine;
+                    _hiPrice = wine.Price;
+                }
+                if (wine.Price < _loPrice)
+                {
+                    _loWine = wine;
+                    _loPrice = wine.Price;
+                }
+            }
+            return (_hiWine, _loWine);
+        }
+    }
+}
+
